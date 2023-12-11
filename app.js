de33f8fb82e25bb81963c0fc86ace29e3ee5f373
alexislam0207 <alexislam0207@gmail.com>
2023-12-11 09:57:43 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const { getAllApiEndpoints } = require("./controllers/endpoints.controllers");
 const {
   handleServerErrors,
@@ -12,6 +13,9 @@ const articlesRoute = require("./routes/articles.routes");
 const commentsRoute = require("./routes/comments.routes");
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/api/topics", topicsRoute);
